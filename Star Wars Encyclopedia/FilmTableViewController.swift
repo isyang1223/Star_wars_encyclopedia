c563fc11978fec4f39248425bd3422f4d16db306
In Soo Yang <isyang1223@gmail.com>
2018-04-08 14:18:57 -0700

    initial commit

---
+++ Star Wars Encyclopedia/FilmTableViewController.swift
@@ -0,0 +1,57 @@
+//
+//  FilmTableViewController.swift
+//  Star Wars Encyclopedia
+//
+//  Created by Ian Yang on 3/19/18.
+//  Copyright © 2018 Ian Yang. All rights reserved.
+//
+
+import UIKit
+
+class FilmTableViewController: UITableViewController {
+    var films = [String]()
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        StarWarsModel.getAllFilms(completionHandler: { // passing what becomes "completionHandler" in the 'getAllPeople' function definition in StarWarsModel.swift
+            data, response, error in
+            do {
+                // Try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    if let results = jsonResult["results"] as? NSArray {
+                        for film in results {
+                            let filmDict = film as! NSDictionary
+                            self.films.append(filmDict["title"]! as! String)
+                        }
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Something went wrong")
+            }
+        })
+        
+    }
+
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return films.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.films[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
---
+++ Star Wars Encyclopedia/PeopleViewController.swift
@@ -0,0 +1,62 @@
+//
+//  ViewController.swift
+//  Star Wars Encyclopedia
+//
+//  Created by Ian Yang on 3/19/18.
+//  Copyright © 2018 Ian Yang. All rights reserved.
+//
+
+import UIKit
+
+class PeopleViewController: UITableViewController {
+    var people = [String]()
+    
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        StarWarsModel.getAllPeople(completionHandler: { // passing what becomes "completionHandler" in the 'getAllPeople' function definition in StarWarsModel.swift
+            data, response, error in
+            do {
+                // Try converting the JSON object to "Foundation Types" (NSDictionary, NSArray, NSString, etc.)
+                if let jsonResult = try JSONSerialization.jsonObject(with: data!, options: JSONSerialization.ReadingOptions.mutableContainers) as? NSDictionary {
+                    if let results = jsonResult["results"] as? NSArray {
+                        for person in results {
+                            let personDict = person as! NSDictionary
+                            self.people.append(personDict["name"]! as! String)
+                        }
+                    }
+                }
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            } catch {
+                print("Something went wrong")
+            }
+        })
+        
+        
+        
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return people.count
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+        cell.textLabel?.text = self.people[indexPath.row]
+        
+        return cell
+    }
+    
+    
+}
+
